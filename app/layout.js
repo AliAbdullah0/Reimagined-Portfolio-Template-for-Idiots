e4d2c019d3ba57e901a176a4032ee03d76cf34b7
Ali Abdullah <aliabdullah10nov2006@gmail.com>
2025-04-08 18:23:14 +0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "Aashir Fasial | Java Developer",
+  title: "Template Portfolio | Next.js Developer",
   description: "Portfolio",
 };
 
